5e9c9f51fb4511cdf8c349420b7ccc65e69d2731
Eugene Chupriyanov <tchu@tchu.ru>
2018-05-18 16:22:54 +0300

    Fixed path to artifact.txt with tarball name

--- build-crypto-pairings.bash
+++ build-crypto-pairings.bash
@@ -98,4 +98,4 @@ fi
 
 cd ${prefix} && \
     tar cvfz ../emotiq-external-libs-${arch}.tgz * && \
-    echo "$(pwd)/emotiq-external-libs-${arch}.tgz" >/tmp/artifact.txt
+    echo "$(pwd)/emotiq-external-libs-${arch}.tgz" >artifact.txt
